4af8fc85df119de1c97c6be3fdb750f9d6dffb88
Adrian <adrianamcovei17@gmail.com>
2023-01-02 23:25:57 +0200

    New mini project added

--- src/EvenDigitSum.java
+++ src/EvenDigitSum.java
@@ -1,6 +1,23 @@
 public class EvenDigitSum {
     public static void main(String[] args) {
-
+        System.out.println("The sum of even digits of  number 3456 is " + getEvenDigitSum(2456));
+        System.out.println("The sum of even digits of  number -3456 is " + getEvenDigitSum(-3456));
     }
-    
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int evenDigitSum = 0;
+        while (number >= 10) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                evenDigitSum += digit;
+            }
+            number /= 10;
+        }
+        if (number % 2 == 0) {
+            evenDigitSum += number;
+        }
+        return evenDigitSum;
+    }
 }
