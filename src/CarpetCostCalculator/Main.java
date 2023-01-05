91375ce48afc764a1bb6278c6e49c6444b517235
Adrian <adrianamcovei17@gmail.com>
2023-01-05 12:16:19 +0200

    OOP mini-project

---
+++ src/CarpetCostCalculator/Main.java
@@ -0,0 +1,11 @@
+package CarpetCostCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+    }
+
+}
